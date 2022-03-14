52ce43632b5e270921fc48870fbdf9bc06939db2
Kayla Villa <kayla@Kaylas-MacBook-Pro.local>
2022-03-14 16:23:01 -0400

    Initial commit

--- autograder/solutions_go_here.py
+++ autograder/solutions_go_here.py
@@ -7,23 +7,24 @@ correct before you get a headstart on the coding. You will not get
 credit for submitting to this autograder. It's just for your testing. 
 This pset will be manually graded
 """
+import numpy as np
 
 def answer_to_1i():
     """
     Return your answer to 1i in a python list, [x, y, theta]
     """
-    return [0, 0, 0]
+    return [((2*(3**(1./2)))+1)/20., (3**(1./2)-2)/20., (np.pi)/60.]
 
 def answer_to_1ii():
     """
     Return your answer to 1ii in a python list, [x, y, theta]
     """
-    return [0, 0, 0]
+    return [((4*np.sqrt(3) - 2)/40) + 3, ((2*np.sqrt(3) + 4)/40) + 4, (21*np.pi)/60]
 
 def answer_to_2():
     """
     Return your answers to 2 in a python list for the values z=0,3,5,8,10
     Each value should be a float
     """
-    return [0, 0, 0, 0, 0]
+    return [.026, .0176, .012198, .0919, .712]
 
