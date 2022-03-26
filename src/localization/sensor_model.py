4d07509dde1ff58c7c2f76f21aee9c5e3321f7f7
PaperCloud <haoni.amy@gmail.com>
2022-03-26 15:17:13 +0000

    added sensor model code + updated test values

--- src/localization/sensor_model.py
+++ src/localization/sensor_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from scan_simulator_2d import PyScanSimulator2D
 
 import rospy
@@ -15,15 +16,16 @@ class SensorModel:
         self.num_beams_per_particle = rospy.get_param("~num_beams_per_particle")
         self.scan_theta_discretization = rospy.get_param("~scan_theta_discretization")
         self.scan_field_of_view = rospy.get_param("~scan_field_of_view")
+        self.lidar_scale_to_map_scale = rospy.get_param("~lidar_scale_to_map_scale")
 
         ####################################
         # TODO
         # Adjust these parameters
-        self.alpha_hit = 0
-        self.alpha_short = 0
-        self.alpha_max = 0
-        self.alpha_rand = 0
-        self.sigma_hit = 0
+        self.alpha_hit = 0.74
+        self.alpha_short = 0.07
+        self.alpha_max = 0.07
+        self.alpha_rand = 0.12
+        self.sigma_hit = 8.0
 
         # Your sensor table will be a `table_width` x `table_width` np array:
         self.table_width = 201
@@ -44,6 +46,7 @@ class SensorModel:
         # Subscribe to the map
         self.map = None
         self.map_set = False
+        self.map_resolution = None
         rospy.Subscriber(
                 self.map_topic,
                 OccupancyGrid,
@@ -69,7 +72,39 @@ class SensorModel:
         returns:
             No return type. Directly modify `self.sensor_model_table`.
         """
-        raise NotImplementedError
+        self.sensor_model_table = np.zeros((self.table_width, self.table_width), dtype=float)
+        z_max = float(self.table_width - 1)
+        index_array = np.where(self.sensor_model_table == 0)
+        z = index_array[0] # z are rows
+        d = index_array[1] # d is column
+
+        z_float = z.astype(float)
+        d_float = d.astype(float)
+
+        # p_hit
+        scalar = (1.0/((2.0*math.pi*self.sigma_hit**2)**0.5))
+        exp = np.exp(-(np.square(z_float-d_float)/(2.0*self.sigma_hit**2)))
+        p_hit = (scalar*exp).reshape((self.table_width, self.table_width))
+        p_hit = p_hit/np.sum(p_hit, axis = 0, keepdims = True, dtype=float)
+        self.sensor_model_table += self.alpha_hit*p_hit
+
+        p_short = ((2.0/d_float)*(1.0-(z_float/d_float))).reshape((self.table_width, self.table_width))
+        # p_short = 0 for d == 0 and z > d
+        p_short[(z>d).reshape((self.table_width, self.table_width))] = 0.0
+        p_short[:, 0] = 0.0
+        # p_short[:, 1:] = p_short[:,1:]/np.sum(p_short[:,1:], axis = 0, keepdims = True)
+        # p_short[:-1, :] = p_short[:-1,:]/np.sum(p_short[:-1,:], axis = 1, keepdims = True)
+        self.sensor_model_table += self.alpha_short*p_short
+
+        p_max = np.zeros((self.table_width, self.table_width), dtype=float)
+        # p_max = 1 for last row 
+        p_max[-1, :] = 1.0
+        self.sensor_model_table += self.alpha_max*p_max
+
+        p_rand = np.ones((self.table_width, self.table_width), dtype=float)/z_max
+        self.sensor_model_table += self.alpha_rand*p_rand
+
+        self.sensor_model_table = self.sensor_model_table/np.sum(self.sensor_model_table, axis = 0, keepdims = True, dtype=float) # columns sum to 1
 
     def evaluate(self, particles, observation):
         """
@@ -103,8 +138,21 @@ class SensorModel:
         # to perform ray tracing from all the particles.
         # This produces a matrix of size N x num_beams_per_particle 
 
+        # scale distances from meters to pixels
+        scalar = self.map_resolution*self.lidar_scale_to_map_scale
         scans = self.scan_sim.scan(particles)
 
+        pixel_scans = scans/scalar
+        pixel_observation = observation/scalar
+
+        pixel_observation = np.clip(pixel_observation, 0, self.table_width)
+        pixel_scans = np.clip(pixel_scans, 0, self.table_width)
+
+        result = np.array(self.sensor_model_table)[pixel_observation.astype(int), pixel_scans.astype(int)]
+
+        return np.power(np.prod(result, axis = 1), 1.0/2.2)
+
+        
         ####################################
 
     def map_callback(self, map_msg):
@@ -133,5 +181,7 @@ class SensorModel:
 
         # Make the map set
         self.map_set = True
+
+        self.map_resolution = map_msg.info.resolution
 
         print("Map initialized")
